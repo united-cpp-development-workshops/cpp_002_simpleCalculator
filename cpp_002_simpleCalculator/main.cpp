e3d390a7332ccd775c0a07ac167cc16aac0ab0c7
EmrecanKaracayir <developer@karacayir.com>
2023-10-29 05:36:13 +0300

    Initial Commit

---
+++ cpp_002_simpleCalculator/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  cpp_002_simpleCalculator
+//
+//  Created by Emrecan Karaçayır on 29/10/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
