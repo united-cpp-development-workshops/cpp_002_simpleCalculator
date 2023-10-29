d886eaac155332a930ca2ad320b950500ac5ea64
EmrecanKaracayir <developer@karacayir.com>
2023-10-29 05:40:25 +0300

    init

--- cpp_002_simpleCalculator/main.cpp
+++ cpp_002_simpleCalculator/main.cpp
@@ -1,14 +1,83 @@
-//
-//  main.cpp
-//  cpp_002_simpleCalculator
-//
-//  Created by Emrecan Karaçayır on 29/10/23.
-//
-
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-  // insert code here...
-  std::cout << "Hello, World!\n";
+const char* prompt()
+{
+  return "Enter an expression with two integers i.e. \"25 + 35\": ";
+}
+
+int add(int a, int b)
+{
+  return a + b;
+}
+
+int subtract(int a, int b)
+{
+  return a - b;
+}
+
+int multiply(int a, int b)
+{
+  return a * b;
+}
+
+int divide(int a, int b)
+{
+  return a / b;
+}
+
+int main()
+{
+  int a = 0, b = 0;
+  char op = '_';
+  int res = 0;
+  while (true)
+  {
+    bool validInput = false;
+    while (!validInput)
+    {
+      std::cout << prompt();
+      while (!(std::cin >> a >> op >> b))
+      {
+        std::cout << "Wrong format try again!" << std::endl;
+        std::cout << prompt();
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+      }
+      switch (op) {
+        case '+':
+          res = add(a, b);
+          validInput = true;
+          break;
+        case '-':
+          res = subtract(a, b);
+          validInput = true;
+          break;
+        case '*':
+        case '.':
+          res = multiply(a, b);
+          validInput = true;
+          break;
+        case '/':
+          res = divide(a, b);
+          validInput = true;
+          break;
+        case '^':
+          res = 1;
+          for (int i = 0; i < b; i++)
+          {
+            res = multiply(res, a);
+          }
+          validInput = true;
+          break;
+        default:
+          std::cout << "Unsupported operation, use {+, -, *, /, ^}!" << std::endl;
+          validInput = false;
+          break;
+      }
+    }
+    std::cout << "The result of " << a << " " << op << " " << b
+    << " is " << res << std::endl;
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+  }
   return 0;
 }
